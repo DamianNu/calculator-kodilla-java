7b955e8739eb152d176df75ba4ff2c43a252b6ef
DamianNu <damian21v@gmail.com>
2023-04-27 18:05:33 +0200

    Poprawa Zadania

--- src/main/java/com/example/calculatorkodilla/Calculator.java
+++ src/main/java/com/example/calculatorkodilla/Calculator.java
@@ -1,18 +1,32 @@
 package com.example.calculatorkodilla;
 
 public class Calculator {
+    private int a;
+    private int b;
+    public Calculator(int a,int b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public int addition(){
+        int result = a + b;
+        System.out.println(result);
+        return result;
+    }
+    public int subtraction(){
+        int result = a - b;
+        System.out.println(result);
+        return result;
+
+    }
+
+}
+
+ class App {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        calculator.addition(64,23);
-        calculator.subtraction(56,57);
+        Calculator calculator = new Calculator(123,10);
+        calculator.addition();
+        calculator.subtraction();
 
     }
-    public void addition(int a,int b){
-        System.out.println(a+b);
-    }
-    public void subtraction(int a,int b){
-        System.out.println(a-b);
-
-    }
-
 }
