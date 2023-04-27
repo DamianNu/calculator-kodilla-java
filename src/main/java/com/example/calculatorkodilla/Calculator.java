27b6f4370a5b9731a4cd000a67d8ce0ed24daa29
DamianNu <damian21v@gmail.com>
2023-04-27 12:30:28 +0200

    2 Commit Change the calculator file

--- src/main/java/com/example/calculatorkodilla/Calculator.java
+++ src/main/java/com/example/calculatorkodilla/Calculator.java
@@ -1,4 +1,17 @@
 package com.example.calculatorkodilla;
 
 public class Calculator {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.addition(64,23);
+        calculator.subtraction(56,57);
+
+    }
+    public void addition(int a,int b){
+        System.out.println(a+b);
+    }
+    public void subtraction(int a,int b){
+        System.out.println(a-b);
+    }
+
 }
